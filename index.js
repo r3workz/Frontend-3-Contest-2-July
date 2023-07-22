fa7beaed6a272f69916bc33856e3b2fd91db29d8
r3 <122603849+r3workz@users.noreply.github.com>
2023-07-23 00:05:45 +0530

    added sound effect and timer end design

--- index.js
+++ index.js
@@ -2,6 +2,52 @@ let timers = [];
 let lastUpdateTime;
 let timersContainer;
 
+const hh = document.getElementById("hh");
+const mm = document.getElementById("mm");
+const ss = document.getElementById("ss");
+
+hh.addEventListener("input", function () {
+	const value = hh.value;
+if(value.length ===0) {
+		hh.value = "";
+		return false;
+	}
+if (!isNaN(value) && value >= 0 && value <= 23) {
+		return true;
+	} else {
+		hh.value = "";
+		return false;
+	}
+});
+
+mm.addEventListener("input", function () {
+	const value = mm.value;
+	if(value.length ===0) {
+		mm.value = "";
+		return false;
+	}
+	if (!isNaN(value) && value >= 0 && value <= 59) {
+		return true;
+	} else {
+		mm.value = "";
+		return false;
+	}
+});
+
+ss.addEventListener("input", function () {
+	const value = ss.value;
+	if(value.length ===0) {
+		ss.value = "";
+		return false;
+	}
+	if (!isNaN(value) && value >= 0 && value <= 59) {
+		return true;
+	} else {
+		ss.value = "";
+		return false;
+	}
+});
+
 function formatTime(time) {
 	const precision = 2;
 
@@ -17,42 +63,74 @@ function formatTime(time) {
 
 function displayTimer() {
 	timersContainer.innerHTML = "";
+	if (timers.length === 0) {
+		timersContainer.innerHTML = `
+    <p id = "noTimers">You have no timers currently!</p>
+  `;
+	} else {
 	timers.forEach((timer, index) => {
 		const timerElement = document.createElement("div");
 		timerElement.className = "timerBox";
+		timerElement.id = `timerBox${index}`;
 		timerElement.innerHTML = `
-        <div class="timerPreText">Time Left: </div>
+        <div class="timerPreText" id="timerPText${index}">Time Left: </div>
         <div class="timer" id="timer${index}">
           ${formatTime(timer.hours)}:${formatTime(timer.minutes)}:${formatTime(timer.seconds)}
         </div>
-        <button class="timerBtn" onclick="deleteTimer(${index})">Delete</button>
+        <button class="timerBtn" id ="deleteBtn${index}" onclick="deleteTimer(${index})">Delete</button>
       `;
 		timersContainer.appendChild(timerElement);
 	});
+	}
 }
 
 function deleteTimer(index) {
 	timers.splice(index, 1);
 	displayTimer();
 }
+function play() {
+	//! "an audio alert of your choice" was mentioned so... :P
+	const audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+	audio.play();
+
+}
 
 function updateTimers() {
 	let now = Date.now();
 	timers.forEach((timer, index) => {
+		const  timerBox = document.getElementById(`timerBox${index}`)
+		const  timerPText = document.getElementById(`timerPText${index}`)
 		const timerElement = document.getElementById(`timer${index}`);
+		const  delBtn = document.getElementById(`deleteBtn${index}`)
 		let totalSeconds = timer.hours * 3600 + timer.minutes * 60 + timer.seconds;
 		if (totalSeconds > 0) {
 			totalSeconds -= ((now - lastUpdateTime) / 1000);
-			timer.hours = Math.round(totalSeconds / 3600);
+			timer.hours = Math.floor(totalSeconds / 3600);
 			totalSeconds -= timer.hours * 3600;
-			timer.minutes = Math.round(totalSeconds / 60);
-			totalSeconds -= timer.minutes * 60;
-			timer.seconds = totalSeconds;
+			if (totalSeconds <= 0) {
+				timer.seconds = 59;
+			} else {
+				timer.seconds = totalSeconds % 60;
+			}
+			timer.minutes = Math.floor(totalSeconds / 60);
+			if(timer.hours<0){
+				timer.hours = 0;
+				timer.minutes=0;
+				timer.seconds=0;
+			}
 			timerElement.textContent = `${formatTime(timer.hours)}:${formatTime(timer.minutes)}:${formatTime(timer.seconds)}`;
 
 		} else {
-			timers.splice(index, 1);
-			displayTimer();
+			timerBox.style.backgroundColor = "#F0F757";
+			timerBox.style.color = "#1D1D23";
+			timerPText.innerHTML = `&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp`;
+			timerElement.textContent = `Timer Is Up`;
+			delBtn.style.backgroundColor = "#39393E";
+			delBtn.style.color = "#ffffff";
+			delBtn.textContent ='Stop';
+			play(); //! "an audio alert of your choice" was mentioned so... :P
+			// timers.splice(index, 1);
+			// displayTimer();
 		}
 	});
 	lastUpdateTime = now;
@@ -80,4 +158,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// Update timers every second
 	setInterval(updateTimers, 1000);
+});
+
+document.getElementById('currentTimersBtn').addEventListener("click", function () {
+	document.getElementById("viewTimers").toggleAttribute('hidden');
 });
